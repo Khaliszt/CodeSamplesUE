187e6681267dd24c275f1930c5bca552e8214f30
Carlos Acevedo <khaliszt@gmail.com>
2020-05-18 22:22:14 +0200

    Update YOURGAMEManager.cpp

--- GameMode/ManagerClass/YOURGAMEManager.cpp
+++ GameMode/ManagerClass/YOURGAMEManager.cpp
@@ -1,7 +1,38 @@
 // Header
 #include "YOURGameManager.h"
 
+// ENGINE
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+
 // YOURGAME
 #include "YOURGAME.h"
 #include "GameModeExample.h"
 
+void AYOURGAMEManager::BeginPlay()
+{
+  Super::BeginPlay();
+  
+  // Get Game Mode
+  if(AGameModeBase* GameMode = UGameplayStatics::GetGameMode(GetWorld()))
+	{
+    // Get Custom Game Mode
+		 if(AGameModeExample* GameModeCast = Cast<AGameModeExample>(GameMode))
+		 {
+			 CustomGameMode  = GameModeCast; // Declared in Header
+			 // Bind the Custom State
+       CustomGameMode->OnGameStateChanged.AddDynamic(this, &YOURGAMEManager::OnGameStateChanged);
+		 }
+	}
+  
+}
+
+void AYOURGAMEManager::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+   Super::EndPlay(EndPlayReason);
+   
+   // Clean Set
+   CustomActorsSet.Empty();
+  
+   // Null pointers
+   CustomGameMode = nullptr;
+}
