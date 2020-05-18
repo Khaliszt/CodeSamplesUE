98be10a1cb0d746b8408cf854e2e1952dfdeaa7c
Carlos Acevedo <khaliszt@gmail.com>
2020-05-19 00:31:54 +0200

    Update CustomActor.h

--- GameMode/CustomActor/CustomActor.h
+++ GameMode/CustomActor/CustomActor.h
@@ -1,1 +1,33 @@
+// Copyright @Khaliszt
 
+// YOURGAME
+#include "YOURGAME.h"
+
+// Forward Declarations
+class AGameModeExample;
+class AYOURGAMEManager;
+  
+UCLASS()
+class YOURGAME_API ACustomActor : public AActor
+{
+  GENERATED_BODY()
+  
+// ENGINE METHODS
+//================
+public:
+  void BeginPlay() override;
+ 
+// YOURGAME METHODS
+//==================
+
+// YOURGAME MEMBERS
+//==================
+protected:
+
+// REFERENCES
+UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category ="ACustomActor|References")
+AYOURGAMEManager* GameManager = nullptr;
+
+UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category ="ACustomActor|References")
+AGameModeExample* GameMode = nullptr;
+};
