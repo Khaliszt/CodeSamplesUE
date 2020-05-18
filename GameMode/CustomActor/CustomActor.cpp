f5ac6ddb07c3798b398bd92e7ec8ae203da1fc0b
Carlos Acevedo <khaliszt@gmail.com>
2020-05-19 00:45:09 +0200

    Create CustomActor.cpp

---
+++ GameMode/CustomActor/CustomActor.cpp
@@ -0,0 +1,17 @@
+// Copyright @Khaliszt
+
+// Header
+#include "GameMode/CustomActor/CustomActor.h"
+
+// ENGINE
+
+// YOURGAME
+#include "GameMode/ManagerClass/YOURGAMEManager.h"
+#include "GameMode/GameModeExample.h"
+
+void ACustomActor::BeginPlay()
+{
+  Super::BeginPlay();
+  
+  
+}
