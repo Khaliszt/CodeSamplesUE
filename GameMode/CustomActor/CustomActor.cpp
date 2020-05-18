b33e60ce6cc06646398496dc06c7fb2f7feaacd4
Carlos Acevedo <khaliszt@gmail.com>
2020-05-19 00:52:00 +0200

    Update CustomActor.cpp

--- GameMode/CustomActor/CustomActor.cpp
+++ GameMode/CustomActor/CustomActor.cpp
@@ -12,21 +12,21 @@
 
 void ACustomActor::BeginPlay()
 {
-  Super::BeginPlay();
+  	Super::BeginPlay();
   
-  // Get Game Mode
-  if(AGameModeBase* GameMode = UGameplayStatics::GetGameMode(GetWorld()))
+  	// Get Game Mode
+	if(AGameModeBase* GameMode = UGameplayStatics::GetGameMode(GetWorld()))
 	{
-    	// Get Custom Game Mode
-		 if(AGameModeExample* GameModeCast = Cast<AGameModeExample>(GameMode))
-		 {
-			  GameMode  = GameModeCast; // Declared in Header
+    		// Get Custom Game Mode
+		if(AGameModeExample* GameModeCast = Cast<AGameModeExample>(GameMode))
+		{
+			GameMode  = GameModeCast; // Declared in Header
         
-        // Add this custom actor to the register of actors
-			  GameMode->YOURGAMEManagerClass->RegisterCustomActor(this); 
+        		// Add this custom actor to the register of actors in the MANAGER (For whatever use)
+			GameMode->YOURGAMEManagerClass->RegisterCustomActor(this); 
        
-       	// COULD ALSO: CustomGameMode->OnGameStateChanged.AddDynamic(this, &ACustomActor::OnGameStateChanged);
-		 }
+       		  	// COULD ALSO: CustomGameMode->OnGameStateChanged.AddDynamic(this, &ACustomActor::OnGameStateChanged);
+		}
 	}
   
   // Example use of the LIBRARY --> ExampleLibrary::FindBestComponentTowardsDirection()
