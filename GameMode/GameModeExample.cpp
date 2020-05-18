e31ec83737a495efa46d048e004569b186bd6706
Carlos Acevedo <khaliszt@gmail.com>
2020-05-18 20:52:56 +0200

    Update GameModeExample.cpp

--- GameMode/GameModeExample.cpp
+++ GameMode/GameModeExample.cpp
@@ -3,3 +3,15 @@
 
 // YOURGAME
 #include "YOURGAMEManager.h"
+
+// ==== EXAMPLE CLASS ====
+ARATSGameModeGameplay::ARATSGameModeGameplay()
+{
+	PrimaryActorTick.bCanEverTick = true; // TODO: Can be false
+	
+	// Get and Set the YOURGAMEManager from the editor's BP Version
+	static ConstructorHelpers::FClassFinder<AYOURGAMEManager> YOURGAMEManagerBPClass (TEXT("/Game/ Your Directory to the class in the Editor"));
+	if (YOURGAMEManagerBPClass.Class != NULL)
+	{
+		DefaultPawnClass = YOURGAMEManagerBPClass.Class;
+}
