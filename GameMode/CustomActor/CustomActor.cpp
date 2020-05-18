1829a8ac6079ec650acfe7a7b077e40e30b31752
Carlos Acevedo <khaliszt@gmail.com>
2020-05-19 00:50:39 +0200

    Update CustomActor.cpp

--- GameMode/CustomActor/CustomActor.cpp
+++ GameMode/CustomActor/CustomActor.cpp
@@ -8,10 +8,27 @@
 // YOURGAME
 #include "GameMode/ManagerClass/YOURGAMEManager.h"
 #include "GameMode/GameModeExample.h"
+#include "ExampleLibrary/ExampleLibrary.h"
 
 void ACustomActor::BeginPlay()
 {
   Super::BeginPlay();
   
+  // Get Game Mode
+  if(AGameModeBase* GameMode = UGameplayStatics::GetGameMode(GetWorld()))
+	{
+    	// Get Custom Game Mode
+		 if(AGameModeExample* GameModeCast = Cast<AGameModeExample>(GameMode))
+		 {
+			  GameMode  = GameModeCast; // Declared in Header
+        
+        // Add this custom actor to the register of actors
+			  GameMode->YOURGAMEManagerClass->RegisterCustomActor(this); 
+       
+       	// COULD ALSO: CustomGameMode->OnGameStateChanged.AddDynamic(this, &ACustomActor::OnGameStateChanged);
+		 }
+	}
   
+  // Example use of the LIBRARY --> ExampleLibrary::FindBestComponentTowardsDirection()
+  // i.e. Get something close to the aiming direction of this actor.
 }
