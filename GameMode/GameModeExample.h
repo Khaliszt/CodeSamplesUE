b2787004adc87391be12815aeba1a4244edc704b
Carlos Acevedo <khaliszt@gmail.com>
2020-05-18 20:42:40 +0200

    Update GameModeExample.h

--- GameMode/GameModeExample.h
+++ GameMode/GameModeExample.h
@@ -8,5 +8,43 @@
 #include "GameModeExample.generated.h"
 
 // ==== EXAMPLE DELEGATE DECLARATION ====
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGameStateChanged, EGameState, NewGameState);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGameStateChanged, EGameState, NewGameState); // ENUM declared in YOURGAME.h !! 
 
+// ==== EXAMPLE CLASS ====
+UCLASS()
+class YOURGAME_API AGameModeExample : public AGameModeBase
+{
+	GENERATED_BODY()
+
+// ENGINE 
+//========
+	
+public:
+
+AGameModeExample();
+
+virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+
+virtual void Tick(float DeltaTime) override;
+
+virtual void BeginPlay() override;
+  
+private:
+
+// YOURGAME METHODS
+//==================
+public:
+void SetGameState(EGameState NewGameState);
+EGameState GetGameState();
+
+// YOURGAME MEMBERS
+//==================
+public:
+// STATES
+UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "GameModeExample|States")
+EGameState CurrentGameState = EGameState::Normal;
+
+protected:
+private:
