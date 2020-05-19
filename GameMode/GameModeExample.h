78b5fbd8c3a16eebb837c59fa7cc4e206559aae0
Carlos Acevedo <khaliszt@gmail.com>
2020-05-19 10:05:16 +0200

    An Example of a GAME MODE with various utilities
    This Game Mode class has a few other actor classes related and spawns one of these, from a BP class or from a C++ class. It will also bind a delegate to these classes in case they want to answer to a generic concept I created as an example. 
    The YOURGAMEManager class makes a generic registry of all the actors that are spawned from the CustomActor class.

--- GameMode/GameModeExample.h
+++ GameMode/GameModeExample.h
@@ -2,15 +2,14 @@
 
 #pragma once
 
+// Header and Minimal
 #include "YOURGAME.h"
-
 #include "CoreMinimal.h"
 #include "Queue.h"
 
-#include "GameModeExample.generated.h"
-
 // YOURGAME
 #include "AYOURGAMEManagerClass.h"
+#include "GameModeExample.generated.h"
 
 // ==== EXAMPLE DELEGATE DECLARATION ====
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGameStateChanged, EGameState, NewGameState); // ENUM declared in YOURGAME.h !! 
